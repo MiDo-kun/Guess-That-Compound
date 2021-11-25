617a6458034583c1d952eb3c785a0437e8950959
Ritz <culanagrv@outlook.com>
2021-11-25 15:52:52 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,72 @@
+# Guess That Compound
+
+## Project Overview
+
+Guess That Compound is a fun and educational web application that challenges players to guess the name of a displayed compound within a short amount of time. The application is designed to help users expand their knowledge in the field of science, specifically in the area of chemistry.
+
+The game is simple: the player is presented with a compound name and a set of multiple-choice answers. The player must select the correct answer within a given time limit to score points. The game can be played in different categories, such as selecting covalent and ionic compounds, with each mode presenting compounds of varying complexity.
+
+In addition to the game, the application includes a learning section where users can explore different concepts in science, such as chemical bonding, molecular structure, and nomenclature. This section provides users with a deeper understanding of compounds and their properties and helps them to expand their knowledge in the field.
+
+Overall, Guess That Compound is a great way to learn and have fun at the same time. It is suitable for students, educators, and anyone who wants to learn more about compounds and their properties.
+
+You can view the live website [here](https://guess-that-compound.web.app/).
+
+## Main Objectives
+
+The main objectives of this project were to:
+
+- Use Firebase Hosting to deploy the project.
+- Gain a better understanding of DOM manipulation in creating dynamic web applications.
+- Learn how to use objects to display information in the document.
+- Improve logical thinking skills when implementing the features of the application.
+
+## Key Features
+
+The Guess That Compound offers the following key features:
+
+- Allows the user to guess the compound within a limited time.
+- Can be played in different modes such as covalent and ionic compounds.
+- Learning section where players can expand their knowledge related to science.
+- Helps users expand their knowledge of compounds and their properties.
+
+## What I Learned
+
+Through this project, I gained the following knowledge and skills:
+
+- Expanded my knowledge of DOM manipulation in creating dynamic web applications.
+- Learned how to import a file and use it as a data input for the questions.
+- Learned how to use objects to better organize and retrieve values efficiently.
+- Started to understand the purpose of using event loop APIs such as setInterval to implement features in the project.
+
+Overall, this project provided me with a solid foundation in modern web development using JavaScript and related technologies. The skills and knowledge I gained will be useful in future web development projects.
+
+## Contributing
+
+Contributions are welcome! If you want to contribute to the project, please follow these guidelines:
+
+1. Fork the repository to your own GitHub account.
+
+2. Clone the repository to your local machine:
+
+   `git clone https://github.com/MiDo-kun/Guess-That-Compound.git`
+
+3. Create a new branch for your changes:
+
+   `git checkout -b your-branch-name`
+
+4. Make your changes to the code.
+
+5. Commit your changes:
+
+   `git commit -m "Your commit message"`
+
+6. Push your changes to your fork:
+
+   `git push origin your-branch-name`
+
+7. Create a pull request from your fork to the main repository.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for more information.
---
+++ scripts/script.js
@@ -0,0 +1,152 @@
+import { ionic, covalent, learn } from './questions.js'
+
+// Screens
+const selectScreen = document.querySelector(".screen--select");
+const quizScreen = document.querySelector(".screen--quiz");
+const backScreen = document.getElementsByClassName('back--screenMenu');
+const backScreenCategory = document.querySelector('.back--screenCategory');
+const timeupScreen = document.querySelector(".screen--timeup");
+const wrongScreen = document.querySelector(".screen--wrong");
+const correctScreen = document.querySelector(".screen--correct");
+
+// Buttons
+const startButton = document.querySelector(".button--start");
+const learnButton = document.querySelector(".button--learn");
+const ionicButton = document.querySelector('.button--ionic');
+const covalentButton = document.querySelector('.button--covalent');
+const popupNextButtons = document.getElementsByClassName("popup__next");
+
+// For Quiz
+const timer = document.querySelector('.quiz__timer');
+const optionButtons = document.getElementsByClassName("button--option");
+const compoundView = document.querySelector(".quiz__compound");
+const compoundInfo = document.querySelector('.compound__information');
+
+// For Learn
+const learnScreen = document.querySelector('.screen--learn');
+const topics = document.querySelector('.topics');
+let quizCategory;
+
+startButton.addEventListener("click", () => {
+   selectScreen.classList.add("screen--visible");
+   // Listen for the user to select category
+   ionicButton.addEventListener('click', () => {
+      quizScreen.classList.add("screen--visible");
+      quizCategory = ionic;
+      showQuestions(ionic); // get the array from import
+   });
+   covalentButton.addEventListener('click', () => {
+      quizScreen.classList.add("screen--visible");
+      quizCategory = covalent;
+      showQuestions(covalent); // get the array from import
+   });
+   // Array Length = Total Questions
+   ionicButton.childNodes[3].textContent = `Total Items: ${ionic.length}`;
+   covalentButton.childNodes[3].textContent = `Total Items: ${covalent.length}`;
+});
+
+learnButton.addEventListener('click', () => {
+   learnScreen.classList.add("screen--visible");
+
+   // Load 'Learn' Contents
+   topics.innerHTML = learn.map((info) => {
+      return ` <div class="topic">
+                  <h2 class="topic__title">${info.title}</h2>
+                  <p class="topic__content">${info.content}</p>
+               </div>`
+   }).join('');
+});
+
+Array.from(backScreen).forEach((btn) => {
+   btn.addEventListener("click", () => {
+      selectScreen.classList.remove("screen--visible");
+      learnScreen.classList.remove("screen--visible");
+   });
+});
+
+const showQuestions = (element) => {
+   // Generate Random Question and Options
+   let seconds = 15;
+   let rand = Math.floor(Math.random() * element.length);
+   let randomQuestion = element[rand].compound;
+   let shuffleOptions = element[rand].options.sort(() => Math.random() - 0.5);
+   timer.innerHTML = seconds;
+
+   // Insert <sub> tag per subscripts to be editable in css
+   let subscripts = /[\u2070-\u209F\u00B2\u00B3\u00B9]/g;
+   let modifiedRandomQuestion = [...randomQuestion];
+   for (let char = 0; char < randomQuestion.length; char++) {
+      if (randomQuestion[char].match(subscripts)) {
+         modifiedRandomQuestion[char] = `<sub>${randomQuestion[char].match(subscripts)}</sub>`;
+      }
+   }
+
+   // Adding Question and Option to the document
+   compoundView.innerHTML = modifiedRandomQuestion.join('');
+   for (let option = 0; option < optionButtons.length; option++) {
+      optionButtons[option].textContent = shuffleOptions[option];
+   }
+
+   // Countdown
+   const start = setInterval(() => {
+      timer.innerHTML = seconds--;
+
+      if (seconds < 0) {
+         clearInterval(start);
+         timeupScreen.classList.add('screen--visible');
+         correctScreen.classList.remove('screen--visible');
+         wrongScreen.classList.remove('screen--visible');
+      }
+   }, 1000);
+
+   // Listen response for user answer
+   Array.from(optionButtons).forEach((option) => {
+      option.addEventListener('click', () => {
+         let userAnswer = option.textContent;
+         let correctAnswer = element[rand].answer;
+         let answerInfo = element[rand].info;
+         checkAnswer(userAnswer, correctAnswer, answerInfo)
+
+         clearInterval(start);
+      });
+   });
+
+   // Back to screen Category
+   backScreenCategory.addEventListener("click", () => {
+      clearInterval(start);
+      quizScreen.classList.remove("screen--visible");
+   });
+
+};
+
+function checkAnswer(user, key, info) {
+   if (user === key) {
+      compoundInfo.innerHTML = info;
+      correctScreen.classList.add('screen--visible');
+      wrongScreen.classList.remove('screen--visible');
+
+      // Remove popup when clicking next
+      Array.from(popupNextButtons).forEach((btns) => {
+         btns.addEventListener('click', () => {
+            quizScreen.classList.add("screen--visible");
+            correctScreen.classList.remove('screen--visible');
+            wrongScreen.classList.remove('screen--visible');
+            showQuestions(quizCategory);
+         });
+      });
+   }
+
+   if (user != key) {
+      wrongScreen.classList.add('screen--visible');
+      correctScreen.classList.remove('screen--visible');
+
+      // Remove popup when clicking next
+      Array.from(popupNextButtons).forEach((btns) => {
+         btns.addEventListener('click', () => {
+            quizScreen.classList.remove("screen--visible");
+            correctScreen.classList.remove('screen--visible');
+            wrongScreen.classList.remove('screen--visible');
+         });
+      });
+   }
+}
